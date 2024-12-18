ba5d807615222edaa5044fa819b355d083f3ffc3
JonathanSaragih <jonathan.srgh@gmail.com>
2024-12-18 15:59:31 +0700

    nyoba inc

--- backend/lib.rs
+++ backend/lib.rs
@@ -8,31 +8,49 @@ type Memory = VirtualMemory<DefaultMemoryImpl>;
 
 // To store global state in a Rust canister, we use the `thread_local!` macro.
 thread_local! {
-    // The memory manager is used for simulating multiple memories. Given a `MemoryId` it can
+    // The memory manager is used for simulating multiple memories. Given a `MemoryId`, it can
     // return a memory that can be used by stable structures.
     static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
         RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
 
     // We store the greeting in a `Cell` in stable memory such that it gets persisted over canister upgrades.
-    static GREETING: RefCell<ic_stable_structures::Cell<String, Memory>> = RefCell::new(
+    static GREETING_TEXT: RefCell<ic_stable_structures::Cell<String, Memory>> = RefCell::new(
         ic_stable_structures::Cell::init(
-            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))), "Hello, ".to_string()
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))), 
+            "Hello, ".to_string()
         ).unwrap()
     );
+
+    static GREETING_COUNT: RefCell<u32> = RefCell::new(0);
+
 }
 
 // This update method stores the greeting prefix in stable memory.
 #[ic_cdk::update]
 fn set_greeting(prefix: String) {
-    GREETING.with_borrow_mut(|greeting| {
-        greeting.set(prefix).unwrap();
+    GREETING_TEXT.with(|greeting| {
+        greeting.borrow_mut().set(prefix).unwrap();
     });
+}
+
+// This update method increments the stored count by the given number.
+#[ic_cdk::update]
+fn inc(prefix: u32) {
+    GREETING_COUNT.with(|greeting| {
+        *greeting.borrow_mut() += prefix; 
+    });
+}
+
+// This query method returns the current value of the incremented count.
+#[ic_cdk::query]
+fn get_count() -> u32 {
+    GREETING_COUNT.with(|count| *count.borrow())
 }
 
 // This query method returns the currently persisted greeting with the given name.
 #[ic_cdk::query]
 fn greet(name: String) -> String {
-    let greeting = GREETING.with_borrow(|greeting| greeting.get().clone());
+    let greeting = GREETING_TEXT.with(|greeting| greeting.borrow().get().clone());
     format!("{greeting}{name}!")
 }
 
