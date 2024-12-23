c536f5a6d2d41ede47c97aeba79aa3940c049266
root <root@LAPTOP-CAIIPHT7>
2024-12-23 14:05:48 +0700

    refactor: update navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -32,7 +32,13 @@ const Navbar = () => {
     <div className={`h-[70px] w-[100vw] fixed transition z-50 duration-500 ease ${scrolled ? (darkMode ? 'bg-black text-white border-b-[1px]' : 'bg-white text-black border-b-[1px]') : (darkMode ? 'bg-transparent text-white' : 'bg-transparent text-black')}`}>
       <div className="h-[70px] w-full flex justify-between items-center px-4 md:px-8">
         <div className="flex items-center">
-          <Link to="/"><img src={logo} className="h-[50px] w-[50px] hover:cursor-pointer" alt="logo" /></Link>
+          <Link to="/">
+            <img 
+              src={logo} 
+              className="h-[50px] w-[50px] hover:cursor-pointer transform transition-transform duration-500 hover:rotate-180" 
+              alt="logo" 
+            />
+          </Link>
           <div className="hidden md:flex items-center ml-4">
             <div className="h-[40px] w-[2px] bg-gray-200"></div>
             <ul className="flex items-center gap-[20px] ml-[20px] font-bold hover:cursor-pointer">
@@ -46,7 +52,7 @@ const Navbar = () => {
         </div>
         <div className="hidden md:flex items-center">
           <div className="h-[45px] w-[400px] pl-[20px] bg-[#d9d9d940] rounded-[10px] border-[0px] flex items-center hover:bg-[#89898930]">
-            <i className="fa-solid fa-magnifying-glass"></i>
+            <i className="fa-solid fa-magnifying-glass transform transition-transform duration-500 hover:rotate-12"></i>
             <input 
               className="h-full w-[80%] ml-[20px] text-[18px] bg-transparent focus:outline-none" 
               placeholder="Search" 
@@ -55,10 +61,10 @@ const Navbar = () => {
         </div>
         <div className="flex items-center gap-[10px]">
           <i 
-            className="fas fa-moon h-[45px] w-[45px] rounded-[10px] bg-[#d9d9d940] flex justify-center items-center hover:bg-[#89898930] cursor-pointer"
+            className="fas fa-moon h-[45px] w-[45px] rounded-[10px] bg-[#d9d9d940] flex justify-center items-center hover:bg-[#89898930] cursor-pointer transform transition-transform duration-500 hover:rotate-12"
             onClick={toggleDarkMode}
           ></i>
-          <i className="fa-solid fa-shopping-cart h-[45px] w-[45px] rounded-[10px] bg-[#d9d9d940] flex justify-center items-center hover:bg-[#89898930]"></i>
+          <i className="fa-solid fa-shopping-cart h-[45px] w-[45px] rounded-[10px] bg-[#d9d9d940] flex justify-center items-center hover:bg-[#89898930] transform transition-transform duration-500 hover:rotate-12"></i>
           <div className="md:hidden">
             <button onClick={toggleMenu}>
               {menuOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
