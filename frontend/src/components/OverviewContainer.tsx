36380c5245c944a0bf91457c7fe89a67ea66c136
JasonFernandoo <j450n.fernando@gmail.com>
2024-12-23 16:34:30 +0700

    refactor: update footer, update overview

--- frontend/src/components/OverviewContainer.tsx
+++ frontend/src/components/OverviewContainer.tsx
@@ -7,12 +7,12 @@ const OverviewContainer = () => {
   const { darkMode } = useDarkMode(); 
   return (
     <div id="overview-content" className={`h-auto w-[100vw] flex transition duration-500 ${darkMode ? 'bg-black text-white' : 'bg-white text-black'}`}>
-      <div className='h-[250vh] w-[30%] flex justify-center'>
+      <div className='h-[253vh] w-[30%] flex justify-center mt-[3%]'>
         <div className='w-[100%] flex text-start'>
           <OverviewNavbar />
         </div>
       </div>
-      <div className='h-auto w-[70%] flex'>
+      <div className='h-auto w-[70%] flex mt-[3%]'>
         <OverviewContent />
       </div>
     </div>
--- frontend/src/components/OverviewHome.tsx
+++ frontend/src/components/OverviewHome.tsx
@@ -21,11 +21,11 @@ const OverviewHome = () => {
   return (
     <div className={`h-[100vh] w-[100vw] flex justify-start items-center transition duration-500 bg-[url('/background2.png')] bg-cover bg-center ${darkMode ? 'bg-black text-white' : 'bg-white text-black'}`}>
         <div className='h-full w-full relative'>
-          <div className='h-auto w-[25%] pt-[2%] pb-[2%] flex bg-[#ffffff60] rounded-[10px] border-[2px] border-[#ffffff80] absolute left-[2.5%] top-[20%] justify-center items-center'>
+          <div className='h-auto w-[25%] pt-[2%] pb-[2%] flex bg-[#ffffff40] rounded-[10px] border-[2px] border-[#ffffff80] absolute left-[2.5%] top-[20%] justify-center items-center'>
             <div className='h-[85%] w-[85%] flex flex-col justify-between text-start'>
                 <h2 className='text-[#898989]'>LEARN HUB</h2>
                 <h1 className='text-[46px] font-bold'>Learn About Aman</h1>
-                <p>Your educational guide to the world of Aman. 
+                <p className='py-[20px]'>Your educational guide to the world of Aman. 
                   Learn how Aman works and how to connect to it. 
                   This page features technical and non-technical articles, 
                   guides, and resources on how Aman leverages blockchain 
